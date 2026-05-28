5d72aa0598db731a394a464db4a8d87cf0475f77
rkritika1508 <rkritika1508@gmail.com>
2026-05-29 04:07:50 +0530

    Added basic setup

---
+++ frontend/src/main.tsx
@@ -0,0 +1,23 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { createRouter, RouterProvider } from "@tanstack/react-router"
+import { StrictMode } from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import { routeTree } from "./routeTree.gen"
+
+const queryClient = new QueryClient()
+
+const router = createRouter({ routeTree })
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router
+  }
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>,
+)
